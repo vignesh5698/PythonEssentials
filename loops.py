027da28fb75ba950d0bd62f2d765964b96640e82
Vignesh <vigneshit13@gmail.com>
2017-12-27 00:58:40 +0530

    Complete Loop Example

--- loops.py
+++ loops.py
@@ -8,5 +8,15 @@ b=0
 for ch in 'PYTHON':
 	b+=1
 	print("Character:",b,ch)
-for x in range(0,100,5):
+for x in range(0,101,5):
 	print("Five Multipliers:",x)	
+num=int(input("Enter the number:"))
+if num%2==0:
+	print("This is Even number")
+else:
+	print("This is Odd Number")
+a=10
+while a>0:
+	print("Decrement:",a)
+	a-=1
+
