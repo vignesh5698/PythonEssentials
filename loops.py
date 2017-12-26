08976fbcbb7d13731c6c0dacfcf5fef2e51ffb5b
Vignesh <vigneshit13@gmail.com>
2017-12-26 23:59:33 +0530

    Loops

--- loops.py
+++ loops.py
@@ -1,3 +1,12 @@
 import random
 a=random.randrange(0,100,5)
-print(a)
+print("Random Number:",a)
+#To print sequence of n numbers
+for a in list(range(10)):
+	print("Value:",a)
+b=0
+for ch in 'PYTHON':
+	b+=1
+	print("Character:",b,ch)
+for x in range(0,100,5):
+	print("Five Multipliers:",x)	
