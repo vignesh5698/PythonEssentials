74f9e90a4dfb5b346f00bd308310911c7efd4f1b
Vignesh <vigneshit13@gmail.com>
2018-01-02 21:52:33 +0530

    Function Example1

---
+++ func1.py
@@ -0,0 +1,19 @@
+def func1(str):
+	pass
+	print(str)
+func1("First Text")
+func1("Sample Text")
+
+for a in "SAMPLE":
+	print(a)
+
+for a in range(5):
+	print(a)
+
+def func2(mylist):
+	pass
+	mylist=[1,2,3]
+	print("Inside Values:",mylist)
+lis=[11,22,33,44]
+func2(lis)
+
