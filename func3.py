a758212edc0be6c15ac8e4ac97fdf02688d378e3
Vignesh <vigneshit13@gmail.com>
2018-01-18 19:38:45 +0530

    Python Function Examples

---
+++ func3.py
@@ -0,0 +1,9 @@
+def fun1():
+	return "SAMPLE TEXT"
+s=fun1()
+print(s)
+
+def params(a):
+	print(a)
+
+params("BHUHAHAHAAA..")
---
+++ func4.py
@@ -0,0 +1,44 @@
+#Nested Function
+def fun1():
+	def fun2():
+		return "Inner Function"
+		
+	print("Outer Function")
+	print(fun2())
+fun1()
+
+def fun3(a):
+	def fun4(b):
+		return a+b
+	return fun4
+a=fun3(3)(4)
+print(a)
+
+def a(q,w):
+	def b(e):
+		return q+w * e
+	return b
+num=a(2,3)(4) 
+print(num)
+
+#Recursion
+def a1(num):
+	if num==1:
+		return 1
+	else:
+		return num * a1(num-1)
+print("Factorial using recursion.....\nThe answer is:",a1(5))
+
+def tailSum(num,accumulator=0):
+	if num==0:
+		return accumulator
+	else:
+		return tailSum(num-1, accumulator+num )
+print("TAIL SUM:",tailSum(10))
+
+#lambda
+v1=lambda x: x * 2
+print(v1(5))
+
+v2=lambda x,y:x+y
+print(v2(5,1000))
